769f1ffdec68631c0a16616451f27bd013f6fa24
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2023-01-03 16:04:21 -0500

    create Card and Collection models

--- magic/models.py
+++ magic/models.py
@@ -9,3 +9,39 @@ class User(AbstractUser):
     bio = models.TextField(max_length=500, blank=True, null=True)
     location = models.CharField(max_length=30, blank=True, null=True)
     birth_date = models.DateField(blank=True, null=True)
+
+
+class Collection(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Card(models.Model):
+    NEAR_MINT = 'NM'
+    POOR = 'PR'
+    MINT = 'MT'
+    GOOD = 'GD'
+
+    CONDITION_CHOICES = [
+        (NEAR_MINT, 'Near Mint'),
+        (POOR, 'Poor'),
+        (MINT, 'Mint'),
+        (GOOD, 'Good')
+    ]
+
+    name = models.CharField(max_length=200)
+    edition = models.CharField(max_length=3)
+    condition = models.CharField(
+        max_length=2,          
+        choices=CONDITION_CHOICES
+    )
+    language = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    # automatically time stamp when a record is created
+    date_updated = models.DateTimeField(auto_now=True)
+    # automatically time stamp when a record is saved (updated) 
+    release_date = models.DateField(blank=True, null=True)
+    # fields are required unless we explicitly make them 
+    # optional with blank=True, null=True
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, null=True)
+    # this article is helpful for implementing image fields https://ordinarycoders.com/blog/article/django-file-image-uploads
